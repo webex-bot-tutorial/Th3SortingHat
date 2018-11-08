d50b39fc5c0efd5d33ca0f97e73e3f0db236c1d0
Eli Sylvia-Lourde <esylvial@cisco.com>
2018-11-08 10:19:34 -0800

    Added Config.py

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import config
+import Webex_Teams as teams
+from flask import Flask, jsonify
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+baseurl = "https://api.ciscospark.com/v1"
+
+bot_auth_token = config.bot_token
+
+bot_id = config.spark_bot_id
+
+headers = {"Content-Type": "application/json",
+           "accept": "application/json",
+           "Authorization": "Bearer %s" % bot_auth_token
+           }
+
+@app.route('/bot_main', methods=['GET','POST'])
+def bot_main():
+
+    # print("hello world")
+
+    spark_hook = request.json
+
+    room_added = str(spark_hook["data"]["roomId"])
+
+    hook_data = spark_hook["data"]
+
+    sessionid = 0
+
+    ###Code that creates greeting message
+    if (str(spark_hook['name']) == 'WebMemberShipCreated') and (hook_data["personId"] == bot_id):
+
+        generalAPIcommands.send_message(room_added, "Hello There! I've been added to a room!")
+
+    elif ((str(spark_hook['name']) == 'WebMessagesSend') and (hook_data["personId"] != bot_id)):
+
+        input_text = mess_content = generalAPIcommands.get_message(hook_data['id'])
+
+        if (len(input_text) == 0):
+            return ''
+
+        sessionid = dialog_flow.make_session_id(hook_data["personId"])
+
+        # Send query to DF:
+        intent_name = dialog_flow.make_dialog_call(input_text, sessionid, room_added)
+
+        # Put your intent_name here!
+        if (intent_name == "example"):
+
+            teams.send_message(room_added, "Hello World!")
+
+if (__name__ == '__main__'):
+    port = int(os.getenv('PORT', 8080))
+    print("Starting App on port %d " %(port))
+    # app.run(debug=True, port=port, host="0.0.0.0")
+    app.run(debug=True, port=port, host="localhost")
---
+++ config.py
@@ -0,0 +1,22 @@
+
+# All variables should be set equal to strings
+
+bot_token = bot_auth_token = None # Delete None and put the Bearer/Auth token here
+
+spark_bot_id = None # Delete None and put the bot id here
+
+CLIENT_ACCESS_TOKEN = None # Delete None and put the token from dialog flow here
+
+# Credentials for accessing LDAP
+
+username = None
+
+password = None
+
+# Information that tells LDAP where it should look
+
+organizational_base = None
+
+mailer_base = None
+
+attributes = None
